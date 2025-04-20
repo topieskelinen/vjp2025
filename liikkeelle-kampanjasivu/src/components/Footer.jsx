dc3e57c7016e609acc7591471c210edea8a62761
Nuutti <nuutti.laitila@gmail.com>
2025-04-20 12:07:55 +0300

    Footer, textbox done

---
+++ liikkeelle-kampanjasivu/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+    return (
+        <div className="footer">
+            <div className="contact_info">
+                <h4>mail: liikkeelle@liikkeelle.fi</h4>
+                <h4>phone: +358 50 1234567</h4>
+            </div>
+            <div className="logos">
+                <img src='/images/instagram_icon.png' alt='instagram logo' />
+                <img src='/images/tiktok_icon.png' alt='tiktok logo' />
+                <img src='/images/facebook_icon.png' alt='facebook logo' />
+            </div>
+        </div>
+    )
+}
---
+++ liikkeelle-kampanjasivu/src/components/Textbox.jsx
@@ -0,0 +1,7 @@
+export default function Textbox(props) {
+    return(
+        <div className="textbox">
+            <h5 className="textbox_text">{props.teksti}</h5>
+        </div>
+    )
+}
