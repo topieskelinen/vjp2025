03852130184a74906993bc81291576ea5bdb3ff7
Nuutti <nuutti.laitila@gmail.com>
2025-04-20 08:16:38 +0300

    Header as a react component

--- liikkeelle-kampanjasivu/src/App.jsx
+++ liikkeelle-kampanjasivu/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Header from './components/header'
 
 function App() {
   return (
     <div className="FrontPage">
       <div className="TitleBox">
         <img src='/images/pandaDab.png' alt='pandadab' />
-        <h1>Liikeelle!</h1>
+        <Header />
         <h4>Pieni askel - Suuri ilo</h4>
       </div>
     </div>
