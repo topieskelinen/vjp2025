64de9db0142dace9cbff7fbce79faa80a841bb06
schumas1 <selma.schumacher@aalto.fi>
2025-05-05 12:13:00 +0300

    tapahtumakiertueen kartta lisätty

---
+++ liikkeelle-kampanjasivu/src/components/TourMap.jsx
@@ -0,0 +1,39 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+
+const customIcon = new L.Icon({
+  iconUrl: '/images/juoksevaPanda.png', 
+  iconSize: [35, 35],
+});
+
+const tourStops = [
+    { city: "Helsinki", date: "15. huhtikuuta", lat: 60.1699, lng: 24.9384 },
+    { city: "Espoo", date: "22. huhtikuuta", lat: 60.2055, lng: 24.6559 },
+    { city: "Vantaa", date: "29. huhtikuuta", lat: 60.2934, lng: 25.0378 },
+    { city: "Tampere", date: "6. toukokuuta", lat: 61.4978, lng: 23.7610 },
+    { city: "Oulu", date: "13. toukokuuta", lat: 65.0121, lng: 25.4651 },
+    { city: "Turku", date: "20. toukokuuta", lat: 60.4518, lng: 22.2666 },
+    { city: "Jyväskylä", date: "27. toukokuuta", lat: 62.2415, lng: 25.7209 },
+    { city: "Lahti", date: "3. kesäkuuta", lat: 60.9827, lng: 25.6615 },
+];
+
+export default function TourMap() {
+  return (
+
+    <div style={{ height: "400px", width: "100%", marginBottom: "30px", marginTop: "30px"}}>
+      <MapContainer center={[62.0, 25.0]} zoom={6} scrollWheelZoom={false} style={{ height: "100%", width: "100%" }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {tourStops.map((stop, idx) => (
+          <Marker key={idx} position={[stop.lat, stop.lng]} icon={customIcon}>
+            <Popup><strong>{stop.city}</strong><br />
+            {stop.date}</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
+
+  );
+}
--- liikkeelle-kampanjasivu/src/main.jsx
+++ liikkeelle-kampanjasivu/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './styleComponents/index.css'
 import App from './App.jsx'
 
+import 'leaflet/dist/leaflet.css';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
