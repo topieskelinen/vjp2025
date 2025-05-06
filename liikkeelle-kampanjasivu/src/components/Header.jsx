eca1008a584010a6a3c6e4c4bc9c670b104902c6
niittyr1 <rosa.niittyviita@aalto.fi>
2025-05-06 22:09:54 +0300

    tyylimuutoksia

--- liikkeelle-kampanjasivu/src/components/Header.jsx
+++ liikkeelle-kampanjasivu/src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <div className="header">
+        <div className="header fade-in">
             <img src='/images/pandaDab.png' alt='pandadab' />
             <h1 className="headerText">Liikkeelle!</h1>
             <h2 className="headerText">Pieni askel - Suuri ilo</h2>
--- liikkeelle-kampanjasivu/src/components/Textbox.jsx
+++ liikkeelle-kampanjasivu/src/components/Textbox.jsx
@@ -1,6 +1,6 @@
 export default function Textbox(props) {
     return(
-        <div className="textbox">
+        <div className="textbox fade-in">
             <h4 className="textbox_text">{props.teksti}</h4>
         </div>
     )
