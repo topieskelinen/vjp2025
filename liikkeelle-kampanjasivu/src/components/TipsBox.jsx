09c6aac560825baa68ae119406b8321a81b94e3c
niittyr1 <rosa.niittyviita@aalto.fi>
2025-05-07 14:19:15 +0300

    konkreettiset vinkit sanan taa avattavaks

---
+++ liikkeelle-kampanjasivu/src/components/TipsBox.jsx
@@ -0,0 +1,20 @@
+export default function TipsBox() {
+    return (
+      <div className="vinkkilaatikko">
+        <h3>Vinkkejä 👇</h3>
+        <ul>
+        <li>🚲 Pyöräilkää yhdessä kouluun tai harrastuksiin</li>
+        <li>🏞️ Tehkää iltakävely ja bongatkaa eläimiä tai värejä - Meidän instagramista @liikkeelle löytyy luontobingo!</li>
+        <li>🎲 Leikkikää yhdessä vanhoja pihapelejä kuten polttopallo tai kirkonrotta</li>
+         <li>🌲 Menkää lähimetsään luontopolulle tai aarteenetsintään</li>
+         <li>🧘 Kokeilkaa joogaa tai venyttelyä yhdessä olohuoneessa</li>
+          <li>🧺 Hyödyntäkää liikkuvia kotitöitä – tehkää vaikka roskien vienti tai koiran ulkoilutus lasten viikottaiseksi vastuuksi!</li>
+          <li>🎲 Ottakaa käyttöön “liikuntayllätyspurkin” – kirjoittakaa lappuihin pieniä aktiviteetteja ja arpokaa yksi per ilta!</li>
+          <li>🚶‍♀️ Pysäköikää auto kauemmas – kävellen viimeiset sadat metrit koululle, harrastuksiin tai kauppaan!</li>
+          <li>💃 Katsokaa yhdessä lasten tanssivideo YouTubesta – ja tanssikaa mukana koko perhe!</li>
+          <li>😉 Tsekatkaa Instagramista @liikkelle lisää vinkkejä</li>
+          <li>🐼 Tulkaa yhdessä mukaan kampanjamme kiertävään liikuntatapahtumaan!</li>
+        </ul>
+      </div>
+    );
+  }
