ebe6023452dd171e032caaca1cb86c6eef253828
Nuutti <nuutti.laitila@gmail.com>
2025-05-07 19:04:14 +0300

    Pävitetty versio kun edellinen merges väärin

--- liikkeelle-kampanjasivu/src/components/EmblaCarousel.jsx
+++ liikkeelle-kampanjasivu/src/components/EmblaCarousel.jsx
@@ -33,7 +33,6 @@ export function EmblaCarousel() {
   useEffect(() => {
     if (isModalOpen) {
       document.body.style.overflow = 'hidden';
-    
     } else {
       document.body.style.overflow = 'auto';
     }
@@ -46,51 +45,82 @@ export function EmblaCarousel() {
         <h2>Sosiaalinen media</h2>
         <div className="embla" ref={emblaRef}>
           <div className="embla__container">
-            <div
-              className="embla__slide"
-              onClick={() =>
-                openModal(
-                  <video className="media-slide" controls autoPlay>
-                    <source src="/videos/tiktok1.mp4" type="video/mp4" />
-                    Selaimesi ei tue videon toistoa.
-                  </video>
-                )
-              }
-            >
+            <div className="embla__slide" onClick={() => openModal(
+              <video className="media-slide" controls>
+                <source src="/videos/tiktok1.mp4" type="video/mp4" />
+                Selaimesi ei tue videon toistoa.
+              </video>
+            )}>
               <video className="media-slide">
                 <source src="/videos/tiktok1.mp4" type="video/mp4" />
                 Selaimesi ei tue videon toistoa.
               </video>
             </div>
-            <div
-              className="embla__slide"
-              onClick={() =>
-                openModal(
-                  <img className="media-slide" src="/images/igkuva1.png" alt="IG kuva" />
-                )
-              }
-            >
-              <img className="media-slide" src="/images/igkuva1.png" alt="IG kuva" />
+            <div className="embla__slide" onClick={() => openModal(
+              <img className="media-slide" src="/images/igkuva6.png" alt="IG kuva" />
+            )}>
+              <img className="media-slide" src="/images/igkuva6.png" alt="IG kuva" />
             </div>
-            <div className="embla__slide" onClick={() => openModal(<img className="media-slide" src="/images/igkuva2.png" alt="IG kuva" />)}>
+            <div className="embla__slide" onClick={() => openModal(
+              <img className="media-slide" src="/images/igkuva7.png" alt="IG kuva" />
+            )}>
+              <img className="media-slide" src="/images/igkuva7.png" alt="IG kuva" />
+            </div>
+            <div className="embla__slide" onClick={() => openModal(
+              <img className="media-slide" src="/images/igkuva8.png" alt="IG kuva" />
+            )}>
+              <img className="media-slide" src="/images/igkuva8.png" alt="IG kuva" />
+            </div>
+            <div className="embla__slide" onClick={() => openModal(
+              <img id='mahdollisuus_ig' className="media-slide" src="/images/igkuva_nuutti.png" alt="IG kuva" />
+            )}>
+              <img id='mahdollisuus_ig' className="media-slide" src="/images/igkuva_nuutti.png" alt="IG kuva" />
+              </div>
+
+            <div className="embla__slide" onClick={() => openModal(
+              <video className="media-slide" controls>
+                <source src="/videos/tiktok2.mp4" type="video/mp4" />
+                Selaimesi ei tue videon toistoa.
+              </video>
+            )}>
+              <video className="media-slide">
+                <source src="/videos/tiktok2.mp4" type="video/mp4" />
+                Selaimesi ei tue videon toistoa.
+              </video>
+            </div>
+            <div className="embla__slide" onClick={() => openModal(
+              <img className="media-slide" src="/images/igkuva2.png" alt="IG kuva" />
+            )}>
               <img className="media-slide" src="/images/igkuva2.png" alt="IG kuva" />
             </div>
-            <div className="embla__slide" onClick={() => openModal(<img className="media-slide" src="/images/igkuva3.png" alt="IG kuva" />)}>
+            <div className="embla__slide" onClick={() => openModal(
+              <img className="media-slide" src="/images/igkuva3.png" alt="IG kuva" />
+            )}>
               <img className="media-slide" src="/images/igkuva3.png" alt="IG kuva" />
             </div>
-            <div className="embla__slide" onClick={() => openModal(<img className="media-slide" src="/images/igkuva4.png" alt="IG kuva" />)}>
+            <div className="embla__slide" onClick={() => openModal(
+              <img className="media-slide" src="/images/igkuva4.png" alt="IG kuva" />
+            )}>
               <img className="media-slide" src="/images/igkuva4.png" alt="IG kuva" />
             </div>
-            <div className="embla__slide" onClick={() => openModal(<img className="media-slide" src="/images/igkuva5.png" alt="IG kuva" />)}>
+            <div className="embla__slide" onClick={() => openModal(
+              <img className="media-slide" src="/images/igkuva5.png" alt="IG kuva" />
+            )}>
               <img className="media-slide" src="/images/igkuva5.png" alt="IG kuva" />
             </div>
-            <div className="embla__slide" onClick={() => openModal(<img className="media-slide" src="/images/facebook1.png" alt="Facebook tapahtuma"/>)}>
-              <img className="media-slide" src="/images/facebook1.png" alt="Facebook tapahtuma" id='facekuva'/>
+            <div className="embla__slide" onClick={() => openModal(
+              <img className="media-slide" src="/images/facebook1.png" alt="Facebook tapahtuma" />
+            )}>
+              <img className="media-slide" src="/images/facebook1.png" alt="Facebook tapahtuma" />
             </div>
+            <div className="embla__slide" onClick={() => openModal(
+              <img className="media-slide" src="/images/igkuva1.png" alt="IG kuva" />
+            )}>
+              <img className="media-slide" src="/images/igkuva1.png" alt="IG kuva" />
             </div>
-            </div>
+          </div>
         </div>
-
+      </div>
       {isModalOpen && (
         <div className="modal-overlay" onClick={closeModal}>
           <div className="modal-content" onClick={(e) => e.stopPropagation()}>
@@ -101,3 +131,4 @@ export function EmblaCarousel() {
       )}
     </>
   );
+}
