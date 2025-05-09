be6715787db7c2e53b25b0a7881e06998f7890df
schumas1 <selma.schumacher@aalto.fi>
2025-05-09 13:28:03 +0300

    sisältöjä

--- liikkeelle-kampanjasivu/src/EventPage.jsx
+++ liikkeelle-kampanjasivu/src/EventPage.jsx
@@ -25,19 +25,13 @@ export default function EventPage() {
                     <>
                       <h2>Ilo-pandan liikuntakiertue</h2>
                       <p>
-                      Koe yhdessä liikkumisen voima! Tervetuloa mukaan Ilo-pandan liikuntakiertueelle, joka kiertää kevään aikana kahdeksalla paikkakunnalla.
-                      Tapahtumassa lapsesi pääsee osallistumaan leikkimieliseen maastojuoksukilpailuun, kokeilemaan uusia leikkejä, liikuntamuotoja ja -lajeja,
-                      tapaamaan urheilutähtiä, sekä tietenkin viettämään aikaa Ilo-pandan kanssa, joka on mukana touhussa!
+                      Koe yhdessä liikkumisen voima! Tervetuloa mukaan Ilo-pandan liikuntakiertueelle, joka kiertää kevään aikana kahdeksalla paikkakunnalla ympäri Suomea. Tapahtumassa lapsesi pääsee kokeilemaan uusia leikkejä, liikuntamuotoja ja -lajeja, esimerkiksi osallistumalla leikkimieliseen maastojuoksukilpailuun. Lapset pääsevät myös tapaamaan vierailevia urheilutähtiä, sekä tietenkin viettämään aikaa Ilo-pandan kanssa, joka aina on mukana touhussa!
                       </p>
                       <p>
-                      Kyseisen viikon vieraileva tähti paljastetaan aina edellisellä viikolla Instagramissa ja Facebookissa. Ota siis nämä seurantaan.
-                      Vierailevina tähtinä toimii Suomen Olympiakomitean kanssa yhteistyönä suomalaisia huippu-urheilijoita, kuten Wilma Murto, Teemu Pukki sekä Saga Vanninen. 
-                      Tule ihastelemaan urheilija-idoleita tapahtumaamme. 
+                      Vierailevina tähtinä toimii Suomen Olympiakomitean kanssa yhteistyönä suomalaisia huippu-urheilijoita, kuten Wilma Murto, Teemu Pukki sekä Saga Vanninen. Kyseisen viikon vieraileva tähti paljastetaan aina edellisellä viikolla Instagramissa ja Facebookissa. Ota siis nämä seurantaan. Tule ihastelemaan urheilija-idoleita tapahtumaamme! 
                       </p>
                       <p>
-                      Ilo-pandan liikuntakiertue on mahdollisimman pienen kynnyksen tapahtuma, maksuton ja suunnattu koko perheelle. 
-                      Tapahtumiimme ovat kaikki tervetulleita lähtökohdista sekä iästä riippumatta. Myös aikuisille on ohjelmaa tapahtuman yhtyedessä. 
-                      Samalla kun lapset nauttivat tapahtuman aktiviteeteista, aikuisille on tarjolla neuvontaa ja vinkkejä lasten liikuntaan liittyen.
+                      Ilo-pandan liikuntakiertue on mahdollisimman pienen kynnyksen tapahtuma, maksuton ja suunnattu koko perheelle. Tapahtumiin on kaikki tervetulleita lähtökohdista sekä iästä riippumatta. Myös aikuisille on ohjelmaa tapahtuman yhtyedessä. Samalla kun lapset nauttivat tapahtuman aktiviteeteista, aikuisille on tarjolla neuvontaa ja vinkkejä lasten liikuntaan liittyen.
                       </p>
                     </>
                   }
--- liikkeelle-kampanjasivu/src/components/PandaQuizButton.jsx
+++ liikkeelle-kampanjasivu/src/components/PandaQuizButton.jsx
@@ -2,8 +2,8 @@ export default function PandaQuizButton() {
     return (
         <div className="pandaQuizButton fade-in">
             <button className="pandaQuizButton">
-                <h2 className="buttonText">Tee panda quiz! – Selvitä millainen panda olet?</h2>
-                <h4 className="buttonText">Testaa lapsesi liikuntatottumuksia leikkimielisesti ja vastaanota käytännön vinkkejä liikunnan lisäämiseen!</h4>
+                <h2 className="buttonText">Tee panda quiz! – Selvitä millainen panda olet</h2>
+                <h4 className="buttonText">Testaa lapsesi liikuntatottumuksia rennolla visalla ja vastaanota käytännön vinkkejä liikunnan lisäämiseen!</h4>
             </button>
         </div>
     )
