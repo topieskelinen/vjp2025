cf008781321240677a99cc5c5bbe1403d75779d8
schumas1 <selma.schumacher@aalto.fi>
2025-05-09 12:46:33 +0300

    sponssit ja vähän muita muutoksia

--- liikkeelle-kampanjasivu/src/App.jsx
+++ liikkeelle-kampanjasivu/src/App.jsx
@@ -33,9 +33,12 @@ function App() {
                   <Header />
                 </div>
                 <Textbox
-                  teksti={`Tervetuloa ilon täyteiseen kampanjaamme, jonka tavoitteena on auttaa aikuisia lisäämään omien alakouluikäisten lastensa liikuntaa ja tuoda hyvää mieltä koko perheelle! "Pieni askel – suuri ilo" -kampanja syntyi huolesta lasten vähenevästä liikkumisesta ja kasvavasta ruutuajasta. Haluamme tarjota innostavia, helposti lähestyttäviä tapoja liikkua enemmän yhdessä. Ilo-panda toimii kampanjamme helposti lähestyttävänä maskottina ja kannustajana, joka muistuttaa meitä siitä, että jokainen askel kohti aktiivisempaa arkea on tärkeä.
+                  teksti={`Tervetuloa ilon täyteiseen kampanjaamme, jonka tavoitteena on auttaa aikuisia lisäämään omien alakouluikäisten lastensa liikuntaa ja tuoda hyvää mieltä koko perheelle!
+                    "Pieni askel – suuri ilo" -kampanja syntyi huolesta lasten vähenevästä liikkumisesta ja kasvavasta ruutuajasta. Haluamme tarjota innostavia, helposti lähestyttäviä tapoja liikkua enemmän yhdessä.
+                    Ilo-panda toimii kampanjamme helposti lähestyttävänä maskottina ja kannustajana, joka muistuttaa meitä siitä, että jokainen askel kohti aktiivisempaa arkea on tärkeä.
                         \n
-                        Kampanjamme näkyy niin somessa kuin ympäristössä. Kampanja tarjoaa aikuisille konkreettisia vinkkejä ja materiaaleja, joiden avulla omien ja läheisten lasten arkeen voi tuoda lisää liikettä yksi askel kerrallaan – syyllistämättä, mutta motivoiden. Tule mukaan kokemaan riemua koko perheen voimin ja luomaan iloa liikkeellä!`}
+                        Kampanjamme näkyy niin somessa kuin ympäristössä. Kampanja tarjoaa aikuisille konkreettisia vinkkejä ja materiaaleja, joiden avulla omien ja läheisten
+                        lasten arkeen voi tuoda lisää liikettä yksi askel kerrallaan – syyllistämättä, mutta motivoiden. Tule mukaan kokemaan riemua koko perheen voimin ja luomaan iloa liikkeellä!`}
                       
                     />
                   
--- liikkeelle-kampanjasivu/src/components/PandaQuizButton.jsx
+++ liikkeelle-kampanjasivu/src/components/PandaQuizButton.jsx
@@ -3,7 +3,7 @@ export default function PandaQuizButton() {
         <div className="pandaQuizButton fade-in">
             <button className="pandaQuizButton">
                 <h2 className="buttonText">Tee panda quiz! – Selvitä millainen panda olet?</h2>
-                <h4 className="buttonText">Testaa lapsesi liikuntottumuksia leikkimielisesti ja vastaanota käytännön vinkkejä liikunnan lisäämiseen!</h4>
+                <h4 className="buttonText">Testaa lapsesi liikuntatottumuksia leikkimielisesti ja vastaanota käytännön vinkkejä liikunnan lisäämiseen!</h4>
             </button>
         </div>
     )
