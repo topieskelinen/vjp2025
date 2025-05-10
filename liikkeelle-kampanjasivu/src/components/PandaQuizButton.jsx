d1fff372fc49bbec02dabc0b96267de1942391c6
schumas1 <selma.schumacher@aalto.fi>
2025-05-10 10:34:25 +0300

    panda-quiz nappi etusivulla toimii

--- liikkeelle-kampanjasivu/src/components/PandaQuizButton.jsx
+++ liikkeelle-kampanjasivu/src/components/PandaQuizButton.jsx
@@ -1,7 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function PandaQuizButton() {
+
+
+    const navigate = useNavigate();
+
+    const goToPage = () => {
+        navigate('/panda-quiz');
+          };
+
     return (
+
         <div className="pandaQuizButton fade-in">
-            <button className="pandaQuizButton">
+            <button className="pandaQuizButton" onClick= {goToPage}>
                 <h2 className="buttonText">Tee panda quiz! – Selvitä millainen panda olet</h2>
                 <h4 className="buttonText">Testaa lapsesi liikuntatottumuksia rennolla visalla ja vastaanota käytännön vinkkejä liikunnan lisäämiseen!</h4>
             </button>
